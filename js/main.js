7586614b0b123559ecd95823988a051215967f04
uixsevriukov@gmail.com <uixsevriukov@gmail.com>
2018-08-05 23:03:05 +0300

    added anim

--- js/main.js
+++ js/main.js
@@ -28,8 +28,36 @@ $(document).ready(function(){
     });
 
 
-    $('.platform, .stage_1, .roadmap, .roadmap .left_col, .roadmap .tabs, .roadmap .item, .about, .advantages, .how_it_works, .team, .faq').addClass("hidden").viewportChecker({
+    $('.platform, .stage_1 .text, .stage_1 .picture .stage1_1, .roadmap, .roadmap .left_col, .roadmap .tabs, .roadmap .item, .about .picture .about_1, .advantages, .how_it_works, .team, .faq').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated fadeInUp',
         offset: 10
     });
+
+    $('.stage_1 .picture .stage1_5, .about .picture .about_5').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeInDown',
+        offset: 10
+    });
+
+    $('.stage_1 .picture .stage1_3, .about .picture .about_4').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeInDown',
+        offset: 100
+    });
+
+    $('.stage_1 .picture .stage1_4, .about .picture .about_3').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeInDown',
+        offset: 130
+    });
+
+    $('.about .picture .about_2').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 0
+    });
+/*
+    var scene = document.getElementById('scene1');
+    var parallaxInstance = new Parallax(scene, {
+       // relativeInput: true
+    });
+
+    parallaxInstance.friction(0, 0.6);*/
+
 });
